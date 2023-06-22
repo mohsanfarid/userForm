9e9705e5d28938916598c0360eb6469e1f46665a
Mohsan <mohsin@techchaps.com>
2023-06-22 13:02:31 +0500

    Adding Validation and resetting logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import logo from './logo.svg';
 import AddUser from './Components/Users/AddUser';
-
 function App() {
   return (
     <div className="">
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from '../UI/Card';
 import classes from './AddUser.module.css'
 import Button from "../UI/Button";
 
 const AddUser = props => {
+    const [enteredUsername, setEnteredusername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
     const AddUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredAge, enteredUsername);
     }
+
+const usernameChangeHandler = (event) => {
+    setEnteredusername(event.target.value);
+};
+
+const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+};
+
     return (
     <Card className={classes.input}>
     <form onSubmit={AddUserHandler}>
-        <lablel htmlFor="username">username</lablel>
-        <input id="username" type="text"/>
+        <label htmlFor="username">username</label>
+        <input id="username" type="text" onChange={usernameChangeHandler}/>
 
-        <lablel htmlFor="age">Age (Years)</lablel>
-        <input id="age" type="number"/>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" onChange={ageChangeHandler}/>
         <Button type="submit">Add User</Button>
     </form>
     </Card>
