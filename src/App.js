3aaec5785d8999484be8f3316feccb82c6004d78
Mohsan <mohsin@techchaps.com>
2023-06-22 13:26:02 +0500

    Managin List using State

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import AddUser from './Components/Users/AddUser';
+import UsersList from './Components/Users/UsersList';
+import {useState} from 'react'; 
 function App() {
+const [usersList, setUsersList] = useState([]);
+
+const AddUserHandler = (uName, uAge) => {
+  setUsersList( (prevUsersList) => {
+    return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()}];
+  });
+}
+
   return (
     <div className="">
-      <AddUser />
+      <AddUser onAddUser={AddUserHandler}/>
+      <UsersList users={usersList}/>
+      
     </div>
   );
 }
