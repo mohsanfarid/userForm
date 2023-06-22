d2cea716417d2fa1f3e8592d52db48dd6eaa0d0d
Mohsan <mohsin@techchaps.com>
2023-06-22 14:07:39 +0500

    Error Handling using states

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -2,18 +2,28 @@ import React, {useState} from "react";
 import Card from '../UI/Card';
 import classes from './AddUser.module.css'
 import Button from "../UI/Button";
+import ErrorModal from '../UI/ErrorModal'
 
 const AddUser = props => {
     const [enteredUsername, setEnteredusername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
     
     
     const AddUserHandler = (event) => {
         event.preventDefault();
         if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid name & age'
+            });
             return;
         }
         if(+enteredAge < 1){
+            setError({
+                title: 'Invalid age',
+                message: 'Please enter a valid age'
+            });
             return;
         }
 
@@ -30,7 +40,13 @@ const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value);
 };
 
+const myErrorHandler = () => {
+    setError(null);
+};
+
     return (
+        <div>
+        {error && <ErrorModal title={error.title} message={error.message} onHandleError={myErrorHandler}/>}
     <Card className={classes.input}>
     <form onSubmit={AddUserHandler}>
         <label htmlFor="username">username</label>
@@ -41,6 +57,7 @@ const ageChangeHandler = (event) => {
         <Button type="submit">Add User</Button>
     </form>
     </Card>
+    </div>
     )
 };
 
